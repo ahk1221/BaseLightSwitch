6e01caa3d9876605c7168b4faef97b5486834447
O-S Marin <K07H@users.noreply.github.com>
2020-05-25 17:12:48 +0200

    Lights state is now saved per base/cyclops instead of per switch.

--- BaseLightSwitch/Main.cs
+++ BaseLightSwitch/Main.cs
@@ -1,11 +1,20 @@
 ﻿namespace BaseLightSwitch
 {
+    using Harmony;
+    using System.Reflection;
+
     public class Main
     {
         public static void Patch()
         {
+            // Patch BaseLightSwitch prefab in the game
             var baseLightSwitchPrefab = new BaseLightSwitchPrefab();
             baseLightSwitchPrefab.Patch();
+
+            // Patch OnProtoSerialize and OnProtoDeserialize methods (used to save and restore bases/cyclops lights state)
+            var harmony = HarmonyInstance.Create("baselightswitch");
+            harmony.Patch(typeof(SubRoot).GetMethod(nameof(SubRoot.OnProtoSerialize), BindingFlags.Public | BindingFlags.Instance), null, new HarmonyMethod(typeof(SubRootFixer).GetMethod(nameof(SubRootFixer.OnProtoSerialize_Postfix), BindingFlags.Public | BindingFlags.Static)));
+            harmony.Patch(typeof(SubRoot).GetMethod(nameof(SubRoot.OnProtoDeserialize), BindingFlags.Public | BindingFlags.Instance), null, new HarmonyMethod(typeof(SubRootFixer).GetMethod(nameof(SubRootFixer.OnProtoDeserialize_Postfix), BindingFlags.Public | BindingFlags.Static)));
         }
 
     }
---
+++ BaseLightSwitch/SubRootRestoreLightsState.cs
@@ -0,0 +1,29 @@
+﻿namespace BaseLightSwitch
+{
+    using UnityEngine;
+
+    /// <summary>Small component attached to bases/cyclops to restore their lights state.</summary>
+    public class SubRootRestoreLightsState : MonoBehaviour
+    {
+        public bool IsLightsOn = true;
+
+        /// <summary>Gets called when this MonoBehaviour wakes up.</summary>
+        public void Awake()
+        {
+            if (enabled)
+                Invoke("RestoreLightState", 3.0f); // We add a small delay before restoring lights state because the cyclops needs few frames to complete its initialization
+        }
+
+        /// <summary>This function gets called by <see cref="Awake"/> method. It restores light state of current base or submarine.</summary>
+        public void RestoreLightState()
+        {
+            if (!enabled)
+                return;
+
+            // Get current base/cyclops and restore its lights state
+            SubRoot subRoot = GetComponent<SubRoot>();
+            if (subRoot != null)
+                subRoot.ForceLightingState(this.IsLightsOn);
+        }
+    }
+}
